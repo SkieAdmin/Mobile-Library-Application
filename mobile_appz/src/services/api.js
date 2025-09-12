6704f88d8b51f6ec521a8b6e4e905e0516ade7f0
Kerneil Rommel Gocotano <kerlemmorsti@gmail.com>
2025-09-12 13:43:01 +0800

    Lol Update

--- mobile_appz/src/services/api.js
+++ mobile_appz/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 
-const BASE_URL = 'http://192.168.0.101:8060/api';
+const BASE_URL = 'http://157.173.198.181:8060/api';
 
 class ApiService {
   constructor() {
